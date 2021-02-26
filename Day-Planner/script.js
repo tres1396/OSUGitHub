2464600df06af2c6f7dc80940c1ccfa72cda9895
Tre Scott <tres1396@gmail.com>
2021-02-26 18:26:07 -0500

    Add folder with files, updated CSS

---
+++ Day-Planner/script.js
@@ -0,0 +1,10 @@
+// Using moment.js to get current date and time 
+var displayCurrentDate = moment().format("dddd, MMMM Do");
+$("#currentDay").text(displayCurrentDate);
+
+// Change timeblocks based on time of day
+var timeOfDayChangeBlock
+
+
+
+document.getElementsByClassName("col-9").classname = "present"
